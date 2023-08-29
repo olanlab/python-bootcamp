b0863749c192211ed78a10ef6d2e191b3d038da0
Olan Samritjiarapon <olan@olanlab.com>
2023-08-29 15:01:22 +0700

    updated webscraping

--- 18-web-scraping/web-scraping-01.py
+++ 18-web-scraping/web-scraping-01.py
@@ -2,14 +2,15 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://www.imdb.com/chart/top"
-
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
 movies = soup.select("td.titleColumn")
 ratings = soup.select("td.imdbRating strong")
 
 for movie, rating in zip(movies, ratings):
+    
     title = movie.a.text
     year = movie.span.text
     rating_value = rating.text.strip()
--- 18-web-scraping/web-scraping-02.py
+++ 18-web-scraping/web-scraping-02.py
@@ -3,8 +3,8 @@ from bs4 import BeautifulSoup
 import csv
 
 url = "https://www.imdb.com/chart/top"
-
-response = requests.get(url)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+response = requests.get(url, headers=headers)
 soup = BeautifulSoup(response.content, "html.parser")
 
 movies = soup.select("td.titleColumn")
