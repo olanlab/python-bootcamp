dee84715ba6aace8cd2501111d490a34561b71c9
Olan Samritjiarapon <olan@olanlab.com>
2023-11-05 09:48:02 +0700

    updated business analytic

---
+++ 16-data/data-visualisation/business-analytics/q-01.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+exchange_rate = pd.read_csv('exchange_rate.csv')
+platform_number = pd.read_csv('platform_number.csv')
+pricing_project_dataset = pd.read_csv('pricing_project_dataset.csv')
+
+
+
